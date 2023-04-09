95726ffdfdba7da5c2ea144310cdaf0972be889c
ArtemNovosel <arteimn@mail.ru>
2023-04-09 19:53:51 +0500

    conftest

--- conftest.py
+++ conftest.py
@@ -8,12 +8,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 BASE_URL = 'https://b2c.passport.rt.ru/'
-EMAIL = 'arteimn@mail.ru'
-PHONE = '9024099791'
-LOGIN = 'rtkid_1679990188472'
-PASSWORD_EMAIL = 'Intqap1032'
-PASSWORD_PHONE = 'INTqap1032'
-PASSWORD_LOGIN = 'INTqap1032'
+EMAIL = 'a*'
+PHONE = '9*'
+LOGIN = 'r*'
+PASSWORD_EMAIL = 'I*'
+PASSWORD_PHONE = 'I*'
+PASSWORD_LOGIN = 'I*'
 
 
 @pytest.fixture(scope='session')
